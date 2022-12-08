89e48231b9a90389d27012c0ca20aac15d636a0e
faresmoalla <73170641+faresmoalla@users.noreply.github.com>
2022-12-08 18:00:48 +0100

    First project

---
+++ lib/car_view.dart
@@ -0,0 +1,47 @@
+// ignore_for_file: public_member_api_docs, sort_constructors_first
+import 'package:flutter/material.dart';
+
+class CarView extends StatelessWidget {
+  String marque ;
+  String model; 
+  String image;
+   CarView(this.marque,this.model,this.image) ;
+
+  
+
+  
+  @override
+  Widget build(BuildContext context) {
+ 
+    return Card(
+          child: Row(
+            children: [
+            Image.asset(image,width: 150,height: 150),
+            Container(
+              margin: EdgeInsets.fromLTRB(50, 0, 0, 0),
+              child:   Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(marque),
+                Text(model)
+              ],
+
+            ),
+
+
+            ),
+           
+
+            ],
+          ),
+
+
+        );
+
+
+  }
+
+
+
+
+}
